078b4576a1168ea1ef6f5214b34414991532177e
Jai Dhingra <117927011+jaidh01@users.noreply.github.com>
2025-03-02 19:50:11 +0530

    Update 1_User.py

--- pages/1_User.py
+++ pages/1_User.py
@@ -24,7 +24,7 @@ st.set_page_config(
 )
 
 class RealtimePoseClassifier:
-    def __init__(self, model_dir='Streamlit-application/model', pose_classifier=mp.solutions.pose):
+    def __init__(self, model_dir='model', pose_classifier=mp.solutions.pose):
         try:
             # Load model components
             self.model = tf.keras.models.load_model(os.path.join(model_dir, 'upward_salute_pose_classifier_v4.0.h5'))
